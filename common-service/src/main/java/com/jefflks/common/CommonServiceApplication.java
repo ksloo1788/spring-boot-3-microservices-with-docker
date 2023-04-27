b294034cfafb23a3aa6ebecbf50233e619e781a9
ksloo1788 <ks_loo@outlook.com>
2023-04-27 15:26:35 +0800

    Initial Version

---
+++ common-service/src/main/java/com/jefflks/common/CommonServiceApplication.java
@@ -0,0 +1,15 @@
+package com.jefflks.common;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
+
+@EnableDiscoveryClient
+@SpringBootApplication
+@RefreshScope
+public class CommonServiceApplication {
+  public static void main(String[] args) {
+    SpringApplication.run(CommonServiceApplication.class, args);
+  }
+}
